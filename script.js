65fc90644534959c785d8cc27237194c77507058
anasimas <ana.simas2109@outlook.com>
2023-03-23 20:51:36 -0300

    fix: correção na validação do campo data de nascimento e de e-mail no arquivo js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function validaFormulario(){
     // validação do campo nome
     var nome = document.cadastro.nome.value;
-    var regExpNome = new RegExp("^[A-zÀ-ü]{2,}([ ]{1}[A-zÀ-ü]{2,})+$")
+    var regExpNome = /[A-zÀ-ü]{2,}([ ]{1}[A-zÀ-ü]{2,})+/;
     if(!regExpNome.test(nome)){
         alert("Preencha o campo Nome corretamente.");
         document.cadastro.nome.focus();
@@ -10,16 +10,17 @@ function validaFormulario(){
 
     //validação do campo data de nascimento
     var dataNascimento = document.cadastro.dtNascimento.value;
-    var regExpDtNasc = new RegExp("^(0[1-9]|[12][0-9]|3[01])[\/.-](0[1-9]|1[012])[\/.-][12][0-9]{3}$")
+    var regExpDtNasc = /[12][0-9]{3}[\/.-](0[1-9]|[12][0-9]|3[01])[\/.-](0[1-9]|1[012])/;
     if(!regExpDtNasc.test(dataNascimento)){
         alert("Preencha o campo Data de nascimento corretamente.");
         document.cadastro.dtNascimento.focus();
+        console.log(dataNascimento);
         return false;
     }
 
     //validação do campo e-mail
     var email = document.cadastro.email.value;
-    var regExpEmail = new RegExp("\S+@\S+\.\S+");
+    var regExpEmail = /\S+@\S+\.\S+/;
     if(!regExpEmail.test(email)){
         alert("Preencha o campo E-mail corretamente.");
         document.cadastro.email.focus();
@@ -28,7 +29,7 @@ function validaFormulario(){
 
     //validação do campo telefone
     var fone = document.cadastro.telefone.value;
-    var regExpFone = new RegExp("^[(][1-9]{2}[)][0-9]{4,5}[-][0-9]{4}$");
+    var regExpFone = /[(][1-9]{2}[)][0-9]{4,5}[-][0-9]{4}/;
     if(!regExpFone.test(fone)){
         alert("Preencha o campo Telefone corretamente.");
         document.cadastro.telefone.focus();
